966abad65192050dae4ef4b7cbe2c3417ef00b87
Mack Stump <mack.stump@gmail.com>
2017-05-21 18:23:26 +0100

    add module parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#[macro_use] extern crate nom;
+
+pub mod parser;
+
+pub use parser::*;
---
+++ src/parser.rs
@@ -0,0 +1,7 @@
+//! The `parser` module contains structures and functions for parsing the VQA
+//! (Vector Quantized Animation) format.
+//!
+//! Information on the VQA format found at:
+//!
+//! * https://wiki.multimedia.cx/index.php/VQA#Technical_Description
+//! * https://multimedia.cx/HC-VQA.TXT
