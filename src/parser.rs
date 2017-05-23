37b27e20b7e2eb67b83c95c2bcb617753ea9be4d
Mack Stump <mack.stump@gmail.com>
2017-05-23 14:32:45 -0400

    add doc dir to project
    Archive https://wiki.multimedia.cx/index.php/VQA and hc-vqa.txt.

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,5 @@
 //! The `parser` module contains structures and functions for parsing the VQA
 //! (Vector Quantized Animation) format.
-//!
-//! Information on the VQA format found at:
-//!
-//! * https://wiki.multimedia.cx/index.php/VQA#Technical_Description
-//! * https://multimedia.cx/HC-VQA.TXT
 
 use nom::{be_u32, le_u8, le_u16, le_u32};
 
